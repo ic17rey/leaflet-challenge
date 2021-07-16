ad7b3a20e01c6e3a2438fc39e0ca3e71f01ad45f
Lindsay Reynolds <78495351+ic17rey@users.noreply.github.com>
2021-07-16 00:32:30 -0400

    Adding more base layers to map

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -103,115 +103,54 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+  });
+
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "satellite-v9",
+    accessToken: API_KEY
+  });
+  // Define baseMaps object for layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap,
+    "Satellite Map": satellitemap
+  };  
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    "Earthquakes > 4.5 Magnitude": earthquakes
+  };
+  
+
+
   var myMap = L.map("map", {
     center: [
       28.4637, -43.7492
     ],
       zoom: 2.5,
-      layers: [streetmap, earthquakes]
-    });
+      layers: [satellitemap, earthquakes]
+  });
 
     legend.addTo(myMap);
-  }
+  
+  // Create a layer control
+  // Pass in baseMaps and overlayMaps
+  // Add layer control
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
+
     
-  
-  
-//For other layers (Leaflet-Step-2)
+
 
 //Satellite: mapbox://styles/mapbox/satellite-v9
 //Outdoors: mapbox://styles/mapbox/outdoors-v11
 //GrayScale: LIGHT?
-
-  // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  //   maxZoom: 18,
-  //   id: "dark-v10",
-  //   accessToken: API_KEY
-  // });
-
-  // Define a baseMaps object to hold our base layers
-  // var baseMaps = {
-  //   "Street Map": streetmap,
-  //   "Dark Map": darkmap
-  // };
-
-  
-  // var overlayMaps = {
-  //   "Earthquakes" : earthquakes
-  // };
-  // Create map, giving it the streetmap and earthquakes layers to display on load
-
-  // Create a layer control
-  // Pass in baseMaps and overlayMaps
-  // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);//
-//}
-
-//For Legend and color scaling? Chloropleth
-// Load in geojson data
-// var geoData = "static/data/Median_Household_Income_2016.geojson";
-
-// var geojson;
-
-// // Grab data with d3
-// d3.json(geoData).then(function(data) {
-
-//   // Create a new choropleth layer
-//   geojson = L.choropleth(data, {
-
-//     // Define what  property in the features to use
-//     valueProperty: "MHI2016",
-
-//     // Set color scale
-//     scale: ["#ffffb2", "#b10026"],
-
-//     // Number of breaks in step range
-//     steps: 10,
-
-//     // q for quartile, e for equidistant, k for k-means
-//     mode: "q",
-//     style: {
-//       // Border color
-//       color: "#fff",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     },
-
-//     // Binding a pop-up to each layer
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
-//         "$" + feature.properties.MHI2016);
-//     }
-//   }).addTo(myMap);
-
-//   // Set up the legend
-//   var legend = L.control({ position: "bottomright" });
-//   legend.onAdd = function() {
-//     var div = L.DomUtil.create("div", "info legend");
-//     var limits = geojson.options.limits;
-//     var colors = geojson.options.colors;
-//     var labels = [];
-
-//     // Add min & max
-//     var legendInfo = "<h1>Median Income</h1>" +
-//       "<div class=\"labels\">" +
-//         "<div class=\"min\">" + limits[0] + "</div>" +
-//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-//       "</div>";
-
-//     div.innerHTML = legendInfo;
-
-//     limits.forEach(function(limit, index) {
-//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-//     });
-
-//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-//     return div;
-//   };
-
-//   // Adding legend to the map
-//   legend.addTo(myMap);
-
-// });
